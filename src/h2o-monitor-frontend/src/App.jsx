f70b4c557c0174a2a723bb2791d52673f37b785d
Angel-crypt <angel.cruz.030905@gmail.com>
2024-07-11 23:29:44 -0600

    Identity

--- src/h2o-monitor-frontend/src/App.jsx
+++ src/h2o-monitor-frontend/src/App.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react';
-import { h2o_monitor_backend } from 'declarations/h2o-monitor-backend';
+import React, { useState } from 'react';
+import * as  h2o_monitor_backend from 'declarations/h2o-monitor-backend';
+
+import Home from './components/Home';
+
+import { createClient } from "@connect2ic/core";
+import { InternetIdentity } from "@connect2ic/core/providers/internet-identity";
+import { ConnectButton, ConnectDialog, Connect2ICProvider } from "@connect2ic/react";
+import { useCanister, useConnect } from "@connect2ic/react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@connect2ic/core/style.css";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    h2o_monitor_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div className='contanier' >
+      {/* <Menu /> */}
+      <Home />
+      {/* <Login /> */}
+    </div>
   );
 }
 
-export default App;
+const client = createClient({
+  canisters: {
+    h2o_monitor_backend,
+  },
+  providers: [
+    new InternetIdentity({ providerUrl: "http://127.0.0.1:8000/?canisterId=bkyz2-fmaaa-aaaaa-qaaaq-cai" })
+  ],
+  globalProviderConfig: {
+    dev: true,
+  },
+});
+
+export default () => (
+  <Connect2ICProvider client={client}>
+    <App />
+  </Connect2ICProvider>
+);
