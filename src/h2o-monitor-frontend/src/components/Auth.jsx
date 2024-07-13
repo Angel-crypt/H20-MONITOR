488fe8223f525fdfeec89b40d51e4c6575403a85
Angel-crypt <angel.cruz.030905@gmail.com>
2024-07-12 23:05:28 -0600

    Front1

--- src/h2o-monitor-frontend/src/components/Auth.jsx
+++ src/h2o-monitor-frontend/src/components/Auth.jsx
@@ -15,6 +15,13 @@ const Auth = () => {
 
     return (
         <div className="auth-container">
+            <div>
+                <h1 className="heading">H20 MONITORING</h1>
+                <h2 style={{ color: '#2a5ea3' }}>Pagos Justos</h2>
+            </div>
+
+            <img src={`${process.env.PUBLIC_URL}/download.png`} alt="H2O" />
+            
             <h1>Por favor, inicie sesión</h1>
             <ConnectButton />
             <ConnectDialog />
